dd8206f65ac28e9609f89e1d616ec2962ae9a743
carol576-cpu <mafongoyacaroline@gmail.com>
2026-04-27 16:14:24 +0200

    Improved js and css

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert("Hello there!");
+function generatePoem(event) {
+  event.preventDefault();
+
+  let input = document.querySelector("#user-input").value;
+  let poem = document.querySelector("#poem");
+
+  poem.innerHTML = `Here is your poem about Love: <br /><br />
+  The Love shines so bright,<br />
+  A gentle glow in silent night.<br />
+  Whispered dreams begin to flow,<br />
+  In places only hearts can know.`;
+}
+
+let form = document.querySelector("#poem-form");
+form.addEventListener("submit", generatePoem);
